969cc85b63be6248c24d438424857bf7235b87ef
Petr Škoda <skodapetr@gmail.com>
2026-04-13 09:48:11 +0200

    Update asset route serving

--- server/http/http-server.mjs
+++ server/http/http-server.mjs
@@ -1,3 +1,5 @@
+import path from "node:path";
+
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import logger from "../logger";
@@ -6,7 +8,7 @@ import { registerHttpRoutes } from "./route.mjs";
 
 export async function createHttpServer(configuration) {
   const application = Fastify({
-    logger: logger,
+    loggerInstance: logger,
     trustProxy: configuration.http.trustProxy,
   });
   await application.register(cors, {
@@ -23,15 +25,23 @@ export function registerRoutes(configuration, server, services) {
 }
 
 function registerAssetsRoutes(configuration, server) {
+  const directory = path.join(import.meta.dirname, "../../assets");
+  logger.info("Serving assets from '%s' directory.", directory);
   server.register(import("@fastify/static"), {
-    root: new URL("../../assets", import.meta.url),
+    root: directory,
     prefix: "/assets/catalog/",
     decorateReply: false
   });
+  //
   if (configuration.server.designSystemFolder) {
-    logger.info("Serving design system assets from the given directory.")
+    const directory = path.join(
+      import.meta.dirname,
+      "/../../",
+      configuration.server.designSystemFolder);
+    logger.info("Serving design system assets from '%s' directory.", directory);
     server.register(import("@fastify/static"), {
-      root: configuration.server.designSystemFolder,
+      // root: new URL("file://" + configuration.server.designSystemFolder),
+      root: directory,
       prefix: "/assets/design-system/",
       decorateReply: false
     });
